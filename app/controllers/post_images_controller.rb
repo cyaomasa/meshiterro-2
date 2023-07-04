671f5ce56818cb6f8e7c5e0f8cded5f4556dcfad
EC2 Default User <ec2-user@ip-172-31-6-194.ap-northeast-1.compute.internal>
2023-07-04 06:18:23 +0000

    [add]sns機能

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -1,4 +1,5 @@
 class PostImagesController < ApplicationController
+  
   def new
     @post_image = PostImage.new
   end
@@ -6,12 +7,15 @@ class PostImagesController < ApplicationController
   def create
     @post_image = PostImage.new(post_image_params)
     @post_image.user_id = current_user.id
-    @post_image.save
+    if @post_image.save
     redirect_to post_images_path
+   else
+    render :new
+   end
   end
 
   def index
-    @post_images = PostImage.all
+    @post_images = PostImage.page(params[:page])
   end
 
   def show
@@ -30,4 +34,5 @@ class PostImagesController < ApplicationController
   def post_image_params
     params.require(:post_image).permit(:shop, :image, :caption)
   end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   get "about" => "homes#about"
   
   resources :post_images, only: [:new, :create, :index, :show, :destroy] do
-    resources :post_comments, only: [:create]
+    resource :favorites, only: [:create, :destroy]
+    resources :post_comments, only: [:create, :destroy]
   end
   resources :users, only: [:show, :edit, :update]
 end
